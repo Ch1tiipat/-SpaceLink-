f6ce5b6629a1175b31e7b2fe492e0d18af9e9188
wannaret <wannaretpond@gmail.com>
2026-07-27 04:05:37 +0700

    SCRUM-19: add users module

---
+++ apps/api/src/users/users.service.ts
@@ -0,0 +1,31 @@
+import { Injectable } from '@nestjs/common';
+import { CreateUserDto } from './dto/create-user.dto';
+import { UpdateUserDto } from './dto/update-user.dto';
+import { PrismaService } from '../prisma/prisma.service';
+
+@Injectable()
+export class UsersService {
+  constructor(private readonly prisma: PrismaService) {}
+
+  // TODO(SCRUM-24): implement Prisma create/update
+  create(_createUserDto: CreateUserDto) {
+    return 'This action adds a new user';
+  }
+
+  findAll() {
+    return this.prisma.user.findMany();
+  }
+
+  findOne(id: number) {
+    return `This action returns a #${id} user`;
+  }
+
+  // TODO(SCRUM-24): implement Prisma create/update
+  update(id: number, _updateUserDto: UpdateUserDto) {
+    return `This action updates a #${id} user`;
+  }
+
+  remove(id: number) {
+    return `This action removes a #${id} user`;
+  }
+}
